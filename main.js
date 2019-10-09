32ca1ee0492329d927fea5b2283cc0e61b2471d7
GRAY JEANNE MEDINA (SHP-IRIS-ISD-OOCLL/MNL) <MEDINGR2@OOCL.COM>
2019-10-09 09:18:08 +0800

    updated populateProductDetailsArray

--- main.js
+++ main.js
@@ -17,31 +17,6 @@ function getUniqueBarCodes (barCodes){
     const listOfUniqueBarCodes = [...new Set(barCodes)]; 
     return listOfUniqueBarCodes;
 };
-// function populateProductDetailsArray (barCodes){
-//     let productList=[];
-//     for(let barcode of barCodes){ 
-//         if(productList.find(product => product.id == barcode)){
-//             productList[productList.findIndex(product => product.id == barcode)].count += 1;
-//         }
-//         else{
-//             let fromProductDB = productDB.find(fromDB => fromDB.id == barcode);
-
-//             productList.push({
-//                 name : fromProductDB.name,
-//                 id : fromProductDB.id,
-//                 price : fromProductDB.price,
-//                 count : 1,
-//                 subTotal: 0
-//             })
-//         }
-    
-//     }
-//     productList.forEach(function(product){
-//         product.subTotal = product.count * product.price;
-//     });
-
-//     return productList;
-// };
 
 function populateProductDetailsArray (barCodes){
     let uniqueBarCodes = getUniqueBarCodes(barCodes);
@@ -51,9 +26,9 @@ function populateProductDetailsArray (barCodes){
         let product = productDB
         .filter(input=>input.id === uniqueBarCode)
         [0];
-        let productCount = productDB
-        .filter(input=> input.id === uniqueBarCode).length;
-        console.log(productCount);
+        let productCount = barCodes
+        .filter(input=> input === uniqueBarCode).length;
+        console.log(barCodes);
         let subtotal = productCount * product.price;
         product.subTotal= subtotal;
         product.count= productCount;
